e07d0edd275d266f10a104c318687ebec30db6a2
Lalit Mohan Singh <128134086+Lalitmohan09@users.noreply.github.com>
2023-12-08 17:30:22 +0530

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,11 +1,13 @@
+let firstLette = "title";
+let secondLetter = "litte";
 function anagram(first, second){
-    let a = first;
-    let b = second;
-    a = first.split("").sort().join("")
-    b = second.split("").sort().join("")
+    let a = first.toLowerCase();
+    let b = second.toLowerCase;
+    a = a.split("").sort().join("")
+    b = b.split("").sort().join("")
     return a===b
 }
 
 
-console.log(anagram("title", "litte"))
+console.log(anagram(firstLetter,secondLetter))
 
